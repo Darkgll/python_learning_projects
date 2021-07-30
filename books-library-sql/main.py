686c7451c820f4f61a45d674102f353fd07dec04
Darkgll <79277744+Darkgll@users.noreply.github.com>
2021-07-30 16:22:46 +0700

    Add files via upload
    Day 63 - Advanced - Databases and with SQLite and SQLAlchemy

---
+++ books-library-sql/main.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.String(250), nullable=False)
+
+
+db.create_all()
+
+
+@app.route('/')
+def home():
+    all_books = db.session.query(Book).all()
+    return render_template('index.html', books=all_books)
+
+@app.route('/del_book')
+def delete_book():
+    book_id = request.args.get('book_id')
+    print(book_id)
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/edit_rating', methods=['GET', 'POST'])
+def edit_rating():
+    if request.method == 'POST':
+        book_id = request.form['book_id']
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form['rating']
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        book_id = request.args.get('book_id')
+        book = Book.query.get(book_id)
+        return render_template('edit_rating.html', book=book)
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        rating = request.form['rating']
+        new_book = Book(title=title, author=author, rating=rating)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
